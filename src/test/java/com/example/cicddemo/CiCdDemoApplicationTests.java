26949148dd0b0d03af039f8446f061ec6f0ddb9c
Paul-Julien Vauthier <pj82491@gmail.com>
2017-10-12 15:20:05 +0800

    Test structure

--- src/test/java/com/example/cicddemo/CiCdDemoApplicationTests.java
+++ src/test/java/com/example/cicddemo/CiCdDemoApplicationTests.java
@@ -2,12 +2,21 @@ package com.example.cicddemo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CiCdDemoApplicationTests {
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
 
 	@Test
 	public void contextLoads() {
